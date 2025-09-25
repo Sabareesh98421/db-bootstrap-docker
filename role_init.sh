89ab9769ef83d1b85fcb594fd5fcff2a5d15d559
Sabareesh98421 <padmanabhansubramanian28@gmail.com>
2025-09-25 14:43:36 +0530

    ADD:add the wrpper shell file to create an ORM user

---
+++ role_init.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#File Name : role_init.sh
+
+
+
+# Wait until PostgreSQL is ready
+
+# until psql -h localhost -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q' 2>/dev/null; do
+#     echo "Waiting for PostgreSQL..."
+#     sleep 2
+# done
+
+# Run SQL with dynamic environment variables
+psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" <<EOSQL
+-- Create user if it doesn't exist
+DO
+\$do\$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = '$ORM_USER') THEN
+      EXECUTE 'CREATE USER $ORM_USER WITH PASSWORD ' || quote_literal('$ORM_PASSWORD');
+   END IF;
+END
+\$do\$;
+
+-- Grant privileges
+GRANT CONNECT ON DATABASE "$POSTGRES_DB" TO "$ORM_USER";
+GRANT CREATE ON SCHEMA public TO "$ORM_USER";
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO "$ORM_USER";
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO "$ORM_USER";
+EOSQL
+
+echo "ORM user setup completed."
